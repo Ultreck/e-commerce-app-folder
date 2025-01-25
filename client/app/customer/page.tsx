0d45a835230f12e70453cdcee42714b00db7d076
Emmanuel Oluwatayese <adetutuemmanueloluwatayese@gmail.com>
2025-01-25 15:05:11 +0100

    fix: I fixed the type error in all file that prompted type error

--- client/app/customer/page.tsx
+++ client/app/customer/page.tsx
@@ -1,12 +1,10 @@
+// import NotFound from '@/components/NotFound'
 import React from 'react'
 
 const page = () => {
   return (
     <div>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga, corporis quaerat, harum tempore neque quo repellendus maiores nemo quae eligendi aspernatur? Illum, nisi deleniti ipsum labore soluta temporibus repellat libero.
-        Quisquam debitis recusandae molestias nam autem, mollitia distinctio dignissimos placeat provident vel, ipsa illo aliquam voluptate natus consectetur quam quo voluptatum ullam molestiae. Voluptas dolores eveniet ipsa quis eaque voluptate?
-        Provident temporibus quaerat fuga alias maxime, iusto repudiandae fugiat eum tenetur delectus sed, distinctio, ipsam quibusdam aut porro hic earum numquam magnam incidunt! Unde quam nihil, rem minus fugiat reiciendis?
-        Pariatur culpa, similique porro nisi repellat officia rerum tenetur veritatis natus, quod, ad iusto amet! Commodi numquam, laboriosam quam optio error aperiam eaque accusamus earum sed voluptate ab praesentium architecto.
+       {/* <NotFound/> */}
     </div>
   )
 }
---
+++ client/components/NotFound.tsx
@@ -0,0 +1,9 @@
+export default function NotFound() {
+    return (
+      <div className="text-center py-10">
+        <h1 className="text-4xl font-bold">404</h1>
+        <p className="text-lg mt-4">Page Not Found</p>
+      </div>
+    );
+  }
+  
